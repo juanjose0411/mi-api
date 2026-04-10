815a58c054b7a6bfe3deab3af90bafd2417bb121
juanjose0411 <juanjosezamudiom@gmail.com>
2026-04-10 13:30:24 -0500

    Add files via upload

---
+++ mi_api.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import google.generativeai as genai
+import os
+
+app = FastAPI()
+
+# Configura tu clave
+genai.configure(api_key="AIzaSyAl9WkBwu_yLlLqcX3obT_M9mVL9mRGLPo")
+
+# ESTO NOS DIRÁ EN EL CMD QUÉ MODELOS TIENES ACTIVOS
+print("--- MODELOS DISPONIBLES EN TU CUENTA ---")
+for m in genai.list_models():
+    if 'generateContent' in m.supported_generation_methods:
+        print(m.name)
+print("---------------------------------------")
+
+# Intentaremos con el nombre más básico y universal
+model = genai.GenerativeModel('gemini-2.5-flash')
+
+@app.get("/preguntar")
+def preguntar_a_gemini(mensaje: str):
+    try:
+        response = model.generate_content(mensaje)
+        return {"respuesta": response.text}
+    except Exception as e:
+        return {"error": str(e)}
